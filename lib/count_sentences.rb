06dc70a4143b0e843e414d92bee03c33ba0c3e07
tstiddy <spaceinvadini99@gmail.com>
2019-05-16 08:35:19 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,19 +6,25 @@ class String
 
   def sentence?
 
-    class dfm
+  self.end_with(".")
 
   end
 
   def question?
 
+    self.end_with("?")
+
   end
 
   def exclamation?
 
+    self.end_with("!")
+
   end
 
   def count_sentences
+
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
 
   end
 end
