a02444064ee54db0478effdf77344b3bacd51cff
moniet <moniet.sawhney@gmail.com>
2019-05-17 12:10:43 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    myarr = self.split(/[?.!]/)
+    myarr.size
+    myarr.reject {|sentence| sentence == ""}.size
+    #binding.pry
   end
 end
